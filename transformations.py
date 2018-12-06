3eb98f31729146f0d76c02ff2b3a2941ee545931
Sotades <anthonyjbateman@gmail.com>
2018-12-06 20:58:23 +0200

    Fixed Defect problem

--- transformations.py
+++ transformations.py
@@ -1,15 +1,30 @@
+""" A bunch of transformations for defect distortion
+
+"""
 import random
 import cv2
 
 def random_null(images):
+    """ Perform a no operation transform on an image.
+
+    Used as a test for calling transforms
+
+    :param images:
+    :return: transformed image
+    """
+
     for img in images:
-        height, width = img.shape[:2]
         yield img
 
 
 def random_stretch_x(images):
+    """ Stretch the defect image randomly by a factor of between 1 and 3 in the x direction
+
+    :param images: the image to be distorted
+    :return: the distorted image
+    """
+
     for img in images:
-        shape = img.shape
         height, width = img.shape[:2]
         scale_factor = random.uniform(1, 3)
         width *= scale_factor
@@ -20,20 +35,40 @@ def random_stretch_x(images):
 
 
 def random_stretch_y(images):
+    """ Stretch the defect image randomly by a factor of between 1 and 3 in the y direction
+
+    :param images: the image to be distorted
+    :return: the distorted image
+    """
     for img in images:
         yield img
 
 
 def random_shear(images):
+    """ Apply a shear transform randomly to the image
+
+    :param images: the image to be distorted
+    :return: the distorted image
+    """
     for img in images:
         yield img
 
 
 def random_rotate(images):
+    """ Apply a rotation randomly to the image
+
+    :param images: the image to be rotated
+    :return: the rotated image
+    """
     for img in images:
         yield img
 
 
 def random_scale(images):
+    """ Apply a scaling randomly to the image
+
+    :param images: the image to be scaled
+    :return: the scaled image
+    """
     for img in images:
         yield img
